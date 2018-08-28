81e065f0edf6cd6b41997e5299770b2cc7458516
Robert Clenshaw <robert.clenshaw@red-gate.com>
2018-08-28 10:30:36 +0100

    Use new wrapper class

--- src/SQLCover/SQLCover/Gateway/DatabaseGateway.cs
+++ src/SQLCover/SQLCover/Gateway/DatabaseGateway.cs
@@ -29,84 +29,59 @@ namespace SQLCover.Gateway
 
         public virtual string GetString(string query)
         {
-            using (var conn = new SqlConnection(_connectionString))
-            {
-                conn.Open();
-                using (var cmd = new SqlCommand(query, conn))
-                {
-                    return cmd.ExecuteScalar().ToString();
-                }
-            }
+            var command = new CommandWrapper(_connectionString, query, _commandTimeout);
+            return command.ExecuteScalar().ToString();
         }
 
         public virtual DataTable GetRecords(string query)
         {
-            using (var conn = new SqlConnection(_connectionString))
+            var command = new CommandWrapper(_connectionString, query, _commandTimeout);
+            using (var reader = command.ExecuteReader())
             {
-                conn.Open();
-                using (var cmd = new SqlCommand(query, conn))
-                {
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        var ds = new DataTable();
-                        ds.Load(reader);
-                        return ds;
-                    }
-                }
+                var ds = new DataTable();
+                ds.Load(reader);
+                return ds;
             }
         }
 
         public virtual DataTable GetTraceRecords(string query)
         {
-            using (var conn = new SqlConnection(_connectionString))
+            var command = new CommandWrapper(_connectionString, query, _commandTimeout);
+            using (var reader = command.ExecuteReader())
             {
-                conn.Open();
-                using (var cmd = new SqlCommand(query, conn))
+                var ds = new DataTable();
+                ds.Columns.Add(new DataColumn("xml"));
+                while (reader.Read())
                 {
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        var ds = new DataTable();
-                        ds.Columns.Add(new DataColumn("xml"));
-                        while (reader.Read())
-                        {
-                            XmlDocument xml = new XmlDocument();
-                            xml.LoadXml(reader[0].ToString());
-                             
-                            var root = xml.SelectNodes("/event").Item(0);
+                    XmlDocument xml = new XmlDocument();
+                    xml.LoadXml(reader[0].ToString());
+                     
+                    var root = xml.SelectNodes("/event").Item(0);
 
-                            var objectId = xml.SelectNodes("/event/data[@name='object_id']").Item(0);
-                            var offset = xml.SelectNodes("/event/data[@name='offset']").Item(0);
-                            var offsetEnd = xml.SelectNodes("/event/data[@name='offset_end']").Item(0);
+                    var objectId = xml.SelectNodes("/event/data[@name='object_id']").Item(0);
+                    var offset = xml.SelectNodes("/event/data[@name='offset']").Item(0);
+                    var offsetEnd = xml.SelectNodes("/event/data[@name='offset_end']").Item(0);
 
-                            root.RemoveAll();
-                                 
-                            root.AppendChild(objectId);
-                            root.AppendChild(offset);
-                            root.AppendChild(offsetEnd);
+                    root.RemoveAll();
+                         
+                    root.AppendChild(objectId);
+                    root.AppendChild(offset);
+                    root.AppendChild(offsetEnd);
 
-                            var row = ds.NewRow();
-                            row["xml"] = root.OuterXml;
-                            ds.Rows.Add(row);
+                    var row = ds.NewRow();
+                    row["xml"] = root.OuterXml;
+                    ds.Rows.Add(row);
 
-                        }
-
-                        return ds;
-                    }
                 }
+
+                return ds;
             }
         }
 
         public void Execute(string query, int timeOut = 30)
         {
-            using (var conn = new SqlConnection(_connectionString))
-            {
-                conn.Open();
-                using (var cmd = new SqlCommand(query, conn))
-                {
-                    cmd.CommandTimeout = timeOut;
-                    cmd.ExecuteNonQuery();
-                }
-            }
+            var command = new CommandWrapper(_connectionString, query, timeOut);
+            command.ExecuteNonQuery();
         }
     }
 }
