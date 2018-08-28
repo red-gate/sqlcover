d8c88aff94f005256844af2b329e07b097eaa3da
Robert Clenshaw <robert.clenshaw@red-gate.com>
2018-08-28 10:26:32 +0100

    Add wrapper around SqlCommand to set up connection

---
+++ src/SQLCover/SQLCover/Gateway/CommandWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLCover.Gateway
+{
+    internal class CommandWrapper
+
+    {
+        private readonly string _connectionString;
+        private readonly string _command;
+        private readonly int _timeout;
+
+        public CommandWrapper(string connectionString, string command, int timeout = 30)
+        {
+            _connectionString = connectionString;
+            _command = command;
+            _timeout = timeout;
+        }
+
+        private T OpenConnectionAndDo<T>(Func<SqlCommand,T> func)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(_command, connection))
+            {
+                command.CommandTimeout = _timeout;
+                connection.Open();
+                return func(command);
+            }
+        }
+
+        public int ExecuteNonQuery() => OpenConnectionAndDo(command => command.ExecuteNonQuery());
+
+        public SqlDataReader ExecuteReader() => OpenConnectionAndDo(command => command.ExecuteReader());
+
+        public object ExecuteScalar() => OpenConnectionAndDo(command => command.ExecuteScalar());
+    }
+}
