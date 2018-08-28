58010ca22340994a0521052df32160417a27a089
Robert Clenshaw <robert.clenshaw@red-gate.com>
2018-08-28 10:43:28 +0100

    Pass timeout into constructor

--- src/SQLCover/test/SQLCover.IntegrationTests/DatabaseSource_IntegrationTests.cs
+++ src/SQLCover/test/SQLCover.IntegrationTests/DatabaseSource_IntegrationTests.cs
@@ -60,13 +60,13 @@ namespace SQLCover.IntegrationTests
         [Test]
         public void Doesnt_Die_When_Finding_Encrypted_Stored_Procedures()
         {
-            var databaseGateway = new DatabaseGateway(TestServerConnectionString, TestDatabaseName);
+            var databaseGateway = new DatabaseGateway(TestServerConnectionString, TestDatabaseName, 15);
             databaseGateway.Execute(@"if not exists (select * from sys.procedures where name = 'enc')
 begin
 	exec sp_executesql N'create procedure enc with encryption 
 	as
 	select 100;'
-end", 15);
+end");
             var source = new DatabaseSourceGateway(databaseGateway);
             var batches = source.GetBatches(null);
             
@@ -87,13 +87,13 @@ end", 15);
         [Test]
         public void Shows_Warnings_When_Definition_Not_Available()
         {
-            var databaseGateway = new DatabaseGateway(TestServerConnectionString, TestDatabaseName);
+            var databaseGateway = new DatabaseGateway(TestServerConnectionString, TestDatabaseName, 15);
             databaseGateway.Execute(@"if not exists (select * from sys.procedures where name = 'enc')
 begin
 	exec sp_executesql N'create procedure enc with encryption 
 	as
 	select 100;'
-end", 15);
+end");
             var source = new DatabaseSourceGateway(databaseGateway);
             var warnings = source.GetWarnings();
             Assert.IsTrue(
