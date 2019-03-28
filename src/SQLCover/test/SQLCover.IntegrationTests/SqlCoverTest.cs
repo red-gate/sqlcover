430703f232cea85d5ea81d20f3e736628a84a688
Michelle.Taylor <michelle.taylor@red-gate.com>
2019-03-28 14:19:50 +0000

    Update this to use a better db name

--- src/SQLCover/test/SQLCover.IntegrationTests/SqlCoverTest.cs
+++ src/SQLCover/test/SQLCover.IntegrationTests/SqlCoverTest.cs
@@ -2,8 +2,8 @@
 {
     public class SQLCoverTest
     {
-        protected const string TestServerConnectionString = "server=is-sql2014.testnet.red-gate.com;integrated security=sspi;initial catalog=LT-DAVIDPc522tk1d";
-        protected const string TestServer2005ConnectionString = "server=is-sql2005.testnet.red-gate.com;User ID=sa;Password=Peruzzi1;initial catalog=LT-DAVIDPbkh03vzx";
-        protected const string TestDatabaseName = "LT-DAVIDPbkh03vzx";
+        protected const string TestServerConnectionString = "server=is-sql2016.testnet.red-gate.com;integrated security=sspi;initial catalog=SqlCoverDatabase";
+        protected const string TestServer2005ConnectionString = "server=is-sql2005.testnet.red-gate.com;User ID=sa;Password=Peruzzi1;initial catalog=master";
+        protected const string TestDatabaseName = "SqlCoverDatabase";
     }
 }
