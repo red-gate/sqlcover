c8774335dc373aec80c99350be3b32afc94c18e5
Sebastian Drela <sebastian.drela@wp.pl>
2023-11-16 01:37:17 +0100

    changed executing sql,so it supports azure

--- src/SQLCover/SQLCover/Source/DatabaseSourceGateway.cs
+++ src/SQLCover/SQLCover/Source/DatabaseSourceGateway.cs
@@ -21,7 +21,7 @@ namespace SQLCover.Source
 
         public SqlServerVersion GetVersion()
         {
-            var compatibilityString = _databaseGateway.GetString("select compatibility_level from sys.databases where database_id = db_id();");
+            var compatibilityString = _databaseGateway.GetString("select compatibility_level from sys.databases where name = DB_NAME();");
             SqlServerVersion res;
             if (Enum.TryParse(string.Format("Sql{0}", compatibilityString), out res))
             {
